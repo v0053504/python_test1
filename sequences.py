ed1f12136ed75f87929ca1208246da682aa77394
Augusto Lengler Konrath <augusto.konrath@universo.univates.br>
2022-03-05 15:18:20 -0300

    added dicts

--- sequences.py
+++ sequences.py
@@ -2,7 +2,21 @@ name = "Harry"
 
 print(name[0])
 
-
+# List:
 names = ["Harry", "Ron", "Hermione"]
 
 print(names[1])
+
+
+# Tuple: sequence of immutable values - cant add new values, ex coordinates:
+coordinate = (10.0, 20.0)
+print(coordinate)
+print(coordinate[1])
+
+# Dict: key-value pairs:
+definition = dict()
+
+definition['apple'] = 'edible fruit'
+definition['speaker'] = 'emmits sound'
+print(definition)
+print(definition['speaker'])
